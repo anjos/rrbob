ea5cad166360e2477a5b78ea0323f9ca30c00558
Andre Anjos <andre.dos.anjos@gmail.com>
2015-06-23 08:02:37 -0300

    Fix documentation

--- doc/conf.py
+++ doc/conf.py
@@ -56,12 +56,12 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'rrpack'
+project = u'rrbob'
 import time
 copyright = u'%s, Idiap Research Institute' % time.strftime('%Y')
 
 # Grab the setup entry
-distribution = pkg_resources.require('rrpack')[0]
+distribution = pkg_resources.require('rrbob')[0]
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -127,7 +127,7 @@ if sphinx.__version__ >= "1.0":
 #html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
-#html_short_title = 'rrpack'
+#html_short_title = 'rrbob'
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
@@ -185,7 +185,7 @@ if sphinx.__version__ >= "1.0":
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'rrpack_doc'
+htmlhelp_basename = 'rrbob_doc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
@@ -199,7 +199,7 @@ latex_font_size = '10pt'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'rrpack.tex', u'Reproducible Research Example in Python',
+  ('index', 'rrbob.tex', u'Reproducible Research Example in Python/Bob',
    u'Biometrics Group, Idiap Research Institute', 'manual'),
 ]
 
@@ -234,7 +234,7 @@ rst_epilog = ''
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'rrpack', u'Reproducible Research Package in Python', [u'Idiap Research Institute'], 1)
+    ('index', 'rrbob', u'Reproducible Research Package in Python/Bob', [u'Idiap Research Institute'], 1)
 ]
 
 # Default processing flags for sphinx
